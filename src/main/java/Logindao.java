ebf510b6afb957576c105141863ddddf232119d9
SagarMane73 <sagarmne7777@gmail.com>
2025-01-02 15:46:23 +0530

    third commited from GitStuffX

--- src/main/java/Logindao.java
+++ src/main/java/Logindao.java
@@ -7,6 +7,9 @@ public class Logindao {
 	{
 		System.out.println("from GitDemo1 from Logindao class1");
 		System.out.println("from GitDemo1 from Logindao class2");
+		
+		System.out.println("from GitStuffX from Logindao class1");
+		System.out.println("from GitStuffX from Logindao class2");
 		String q="select * from Login1 where lmail=? and lpwd=?";
 		PreparedStatement pst= new dbadapter().dbconnect().prepareStatement(q); //here "con" reference jaisa connection get kiya hai
 		pst.setString(1, lp.getLmail());
