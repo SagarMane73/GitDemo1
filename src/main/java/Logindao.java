4b50abda563a6c8cea8af0f465220971f5c814b5
SagarMane73 <sagarmne7777@gmail.com>
2025-01-02 15:17:31 +0530

    this is commit from GitDemo folder

--- src/main/java/Logindao.java
+++ src/main/java/Logindao.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Logindao {
 	public String validate(Loginpojo lp) throws SQLException
 	{
-		
+		System.out.println("from GitDemo1 from Logindao class");
 		String q="select * from Login1 where lmail=? and lpwd=?";
 		PreparedStatement pst= new dbadapter().dbconnect().prepareStatement(q); //here "con" reference jaisa connection get kiya hai
 		pst.setString(1, lp.getLmail());
