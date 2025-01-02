ce4550458cdb73a21798a044cf351df9cc611a9b
SagarMane73 <sagarmne7777@gmail.com>
2025-01-02 16:03:12 +0530

    architect changes from GitDemo1

--- src/main/java/Logindao.java
+++ src/main/java/Logindao.java
@@ -10,6 +10,11 @@ public class Logindao {
 		
 		System.out.println("from GitStuffX from Logindao class1");
 		System.out.println("from GitStuffX from Logindao class2");
+		
+		System.out.println("from GitDemo1 from Logindao class2 architech changes");
+		System.out.println("from GitDemo1 from Logindao class2 architech changes");
+		
+		
 		String q="select * from Login1 where lmail=? and lpwd=?";
 		PreparedStatement pst= new dbadapter().dbconnect().prepareStatement(q); //here "con" reference jaisa connection get kiya hai
 		pst.setString(1, lp.getLmail());
