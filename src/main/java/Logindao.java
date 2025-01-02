43e6a15994b64d8629bc9752bf822394df19a53e
SagarMane73 <sagarmne7777@gmail.com>
2025-01-02 16:14:07 +0530

    commited

--- src/main/java/Logindao.java
+++ src/main/java/Logindao.java
@@ -14,6 +14,7 @@ public class Logindao {
 		System.out.println("from GitDemo1 from Logindao class2 architech changes");
 		System.out.println("from GitDemo1 from Logindao class2 architech changes");
 		
+		System.out.println("from GitDemo1 from Logindao class2 architech2 changes");
 		
 		String q="select * from Login1 where lmail=? and lpwd=?";
 		PreparedStatement pst= new dbadapter().dbconnect().prepareStatement(q); //here "con" reference jaisa connection get kiya hai
