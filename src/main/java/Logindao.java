3a1a13374efb802f174171ed2391e1e316d0acdc
SagarMane73 <sagarmne7777@gmail.com>
2025-01-02 16:50:20 +0530

    sagar added to GitStuffx

--- src/main/java/Logindao.java
+++ src/main/java/Logindao.java
@@ -16,6 +16,8 @@ public class Logindao {
 		
 		System.out.println("from GitDemo1 from Logindao class2 architech2 changes");
 		System.out.println("yyyyy");
+		System.out.println("sagar");
+		
 		String q="select * from Login1 where lmail=? and lpwd=?";
 		PreparedStatement pst= new dbadapter().dbconnect().prepareStatement(q); //here "con" reference jaisa connection get kiya hai
 		pst.setString(1, lp.getLmail());
